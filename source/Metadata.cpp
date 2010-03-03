d1dc94e86babf7733816579395bd3eb3d7543c6a
Josh Ferguson <Josh@Besquared.local>
2010-03-03 00:19:11 -0800

    metadata/bdb wrapper for various things

---
+++ source/Metadata.cpp
@@ -0,0 +1,174 @@
+/*
+ *  Metadata.cpp
+ *  Flow
+ *
+ *  Created by Josh Ferguson on 12/14/09.
+ *  Copyright 2009 Cube Tree Labs, LLC. All rights reserved.
+ *
+ */
+
+#include "Metadata.h"
+
+Retsu::Metadata::Metadata(const string& path, const string& name) {
+	this->path = path;
+	this->name = name;
+	this->database = tcbdbnew();
+}
+
+Retsu::Metadata::~Metadata() {
+	this->Close();
+  tcbdbdel(this->database);
+}
+
+bool Retsu::Metadata::Create(const string& path, const string& name) {
+	TCBDB* database = tcbdbnew();
+	
+	tcbdbtune(database, -1, -1, -1, -1, -1, BDBTLARGE | BDBTDEFLATE);
+	if(tcbdbopen(database, Retsu::Metadata::Path(path, name).c_str(), BDBOWRITER | BDBOCREAT)) {
+		tcbdbclose(database);
+		tcbdbdel(database);
+		return true;
+	} else {
+		tcbdbdel(database);
+		return false;
+	}
+}
+
+std::string Retsu::Metadata::Path(const string& path, const string& name) {
+  return path + "/" + name + ".tcb";
+}
+
+bool Retsu::Metadata::OpenReader() {
+  return this->Open(BDBOREADER);
+}
+
+bool Retsu::Metadata::OpenWriter() {
+  return this->Open(BDBOWRITER | BDBOCREAT);
+}
+
+bool Retsu::Metadata::Truncate() {
+	if(this->Open(BDBOWRITER | BDBOTRUNC)) {
+		this->Close();
+		return true;
+	} else {
+		return false;
+	}
+}
+
+bool Retsu::Metadata::Open(int mode) {
+	return tcbdbopen(this->database, Retsu::Metadata::Path(this->path, this->name).c_str(), mode);
+}
+
+bool Retsu::Metadata::Close() {
+	if(tcbdbrnum(this->database) % (1<<16) == 0) {
+		tcbdboptimize(database, -1, -1, -1, -1, -1, BDBTLARGE | BDBTDEFLATE);
+	}
+	
+	return tcbdbclose(this->database);
+}
+
+bool Retsu::Metadata::Get(const string& key, string& result) {
+	int b_size;
+	void* buffer;
+	
+	buffer = tcbdbget(this->database, key.c_str(), key.size(), &b_size);
+	
+	if(buffer == NULL) {
+		return false;
+	} else {
+		result.assign((char*)buffer, b_size);
+		free(buffer);
+		return true;
+	}
+}
+
+bool Retsu::Metadata::Get(const string& key, set<string>& results) {
+	TCLIST* values = tcbdbget4(this->database, key.c_str(), key.size());
+	
+	if(values != NULL) {
+		const char* value;
+		size_t list_size = tclistnum(values);
+    for(size_t i = 0; i < list_size; i++) {
+			value = tclistval2(values, i);
+			if(value != NULL) { results.insert(value); }
+    }
+    tclistdel(values);
+		return true;
+  } else {
+		return false;
+	}
+}
+
+bool Retsu::Metadata::Get(const string& key, vector<string>& results) {
+	TCLIST* values = tcbdbget4(this->database, key.c_str(), key.size());
+	
+	if(values != NULL) {
+		const char* value;
+		size_t list_size = tclistnum(values);
+    for(size_t i = 0; i < list_size; i++) {
+			value = tclistval2(values, i);
+			if(value != NULL) { results.push_back(value); }
+    }
+    tclistdel(values);
+		return true;
+  } else {
+		return false;
+	}
+}
+
+bool Retsu::Metadata::Get(const string& key, RIDList& results) {
+	int size = (int)sizeof(RecordID);
+	void* records = tcbdbget(this->database, key.c_str(), key.size(), &size);
+	
+	if(records != NULL) {
+		size_t count = size / sizeof(RecordID);
+		results.insert(results.end(), (RecordID*)records, (RecordID*)records + count);
+		free(records);
+		return true;
+	} else {
+		return false;
+	}
+}
+
+bool Retsu::Metadata::Put(const string& key, const string& value) {
+	return tcbdbput(this->database, key.c_str(), key.size(), value.c_str(), value.size());
+}
+
+bool Retsu::Metadata::PutDup(const string& key, const string& value) {
+	return tcbdbputdup(this->database, key.c_str(), key.size(), value.c_str(), value.size());
+}
+
+bool Retsu::Metadata::PutCat(const string& key, const RecordID& value) {
+	return tcbdbputcat(this->database, key.c_str(), key.size(), &value, sizeof(RecordID));
+}
+
+bool Retsu::Metadata::Add(const string& key, const int value, int& result) {
+	result = tcbdbaddint(this->database, key.c_str(), key.length(), value);
+	
+	if(isnan(result)) {
+		return false;
+	} else {
+		return true;
+	}
+}
+
+bool Retsu::Metadata::Add(const string& key, const double value, double& result) {
+	result = tcbdbadddouble(this->database, key.c_str(), key.length(), value);
+	
+	if(isnan(result)) {
+		return false;
+	} else {
+		return true;
+	}
+}
+
+/*
+ * Returns the last error to occur on the database
+ */
+std::string Retsu::Metadata::Error() {
+	return string(tcbdberrmsg(tcbdbecode(this->database)));
+}
+
+int Retsu::Metadata::ErrorCode() {
+	return tcbdbecode(this->database);
+}
---
+++ source/Metadata.h
@@ -0,0 +1,56 @@
+/*
+ *  Metadata.h
+ *  Flow
+ *
+ *  Created by Josh Ferguson on 12/14/09.
+ *  Copyright 2009 Cube Tree Labs, LLC. All rights reserved.
+ *
+ */
+
+#ifndef _RETSU_METADATA_H_
+#define _RETSU_METADATA_H_
+
+#include "Common.h"
+#include "Record.h"
+#include "RIDList.h"
+
+namespace Retsu {
+  using namespace std;
+
+  class Metadata {
+  protected:
+    string path;
+    string name;
+    TCBDB *database;
+    
+  public:
+    Metadata(const string& path, const string& name);
+    virtual ~Metadata();
+    
+    virtual bool OpenReader();
+    virtual bool OpenWriter();
+    virtual bool Open(int mode);
+    virtual bool Close();
+    virtual bool Truncate();
+    
+    virtual bool Get(const string& key, string& result);
+    virtual bool Get(const string& key, set<string>& results);
+    virtual bool Get(const string& key, vector<string>& results);
+    virtual bool Get(const string& key, RIDList& results);
+    
+    virtual bool Put(const string& key, const string& value);
+    virtual bool PutDup(const string& key, const string& value);
+    virtual bool PutCat(const string& key, const RecordID& record);
+    
+    virtual bool Add(const string& key, const int value, int& result);
+    virtual bool Add(const string& key, const double value, double& result);
+    
+    string Error();
+    int ErrorCode();
+    
+    static bool Create(const string& path, const string& name);
+    static string Path(const string& path, const string& name);
+  };
+}
+
+#endif
