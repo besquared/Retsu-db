03807f256f4aa06bf878c59dcfff1bc1984702ae
Josh Ferguson <Josh@Besquared.local>
2010-03-31 23:51:59 -0700

    space maybe?

--- source/statistics/location.h
+++ source/statistics/location.h
@@ -18,7 +18,6 @@ namespace Retsu {
     double geometric_mean(const std::vector<double>& values);
     double harmonic_mean(const std::vector<double>& values);
     double trimmed_mean(const std::vector<double>& values);
-    
     double median(const std::vector<double>& values);
     double mode(const std::vector<double>& values);
     double max(const std::vector<double>& values);
